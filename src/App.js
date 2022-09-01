3b63373a1ab138a1e55e3bd200e982f6dc2e2092
Danny Marks <dannywmarks@gmail.com>
2022-09-01 12:52:56 -0700

    fixed optional chaining issue reinstalling nodem

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route} from 'react-router-dom'
-import { Box } from '@mui/material'; // Basic DIV element
-import {Navbar, Feed, ChannelDetail, VideoDetail, SearchFeed} from './components'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from '@mui/material';
 
-const App = () => {
-  return (
-   <BrowserRouter>
-    <Box sx={{ backgroundColor: '#000'}}>
+import { ChannelDetail, VideoDetail, SearchFeed, Navbar, Feed } from './components';
+
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: '#000' }}>
       <Navbar />
       <Routes>
-        <Route path="/" exact element={<Feed />} />
-        <Route path="/video/:id" element={<VideoDetail />} />
-        <Route path="/channel/:id" element={<ChannelDetail />} />
-        <Route path="/search/:searchTerm" element={<SearchFeed />} />
+        <Route exact path='/' element={<Feed />} />
+        <Route path='/video/:id' element={<VideoDetail />} />
+        <Route path='/channel/:id' element={<ChannelDetail />} />
+        <Route path='/search/:searchTerm' element={<SearchFeed />} />
       </Routes>
     </Box>
-   </BrowserRouter>
-  )
-}
+  </BrowserRouter>
+);
 
-export default App
+export default App;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import { Stack } from "@mui/material";
+
 import { categories } from "../utils/constants";
 
-
-
-const Sidebar = ({setSelectedCategory, selectedCategory}) => (
+const Categories = ({ selectedCategory, setSelectedCategory }) => (
   <Stack
     direction="row"
     sx={{
@@ -17,17 +16,21 @@ const Sidebar = ({setSelectedCategory, selectedCategory}) => (
       <button
         className="category-btn"
         onClick={() => setSelectedCategory(category.name)}
-        key={category.name}
         style={{
           background: category.name === selectedCategory && "#FC1503",
           color: "white",
         }}
+        key={category.name}
       >
-        <span style={{color: category.name === selectedCategory ? 'white' : 'red', marginRight: '15px'}}>{category.icon}</span>
-        <span style={{ opacity: category.name === selectedCategory ? '1' : '0.8'}}>{category.name}</span>
+        <span style={{ color: category.name === selectedCategory ? "white" : "red", marginRight: "15px" }}>
+          {category.icon}
+        </span>
+        <span style={{ opacity: category.name === selectedCategory ? "1" : "0.8" }}>
+          {category.name}
+        </span>
       </button>
     ))}
   </Stack>
 );
 
-export default Sidebar;
+export default Categories;
